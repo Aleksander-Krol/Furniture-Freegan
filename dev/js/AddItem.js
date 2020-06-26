caba85bca10ceebee0161f0dccab5bea513392b7
Aleksander-Krol <62897137+Aleksander-Krol@users.noreply.github.com>
2020-06-26 13:23:03 +0200

    dzialajaca infastruktura

---
+++ dev/js/AddItem.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+export const AddItem = () => {
+    return (
+       <form>Hello
+
+           
+       </form>
+    )
+}
---
+++ dev/js/app.js
@@ -0,0 +1,6 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import {AddItem} from "./AddItem";
+
+const App = () => <AddItem/>
+ReactDOM.render(<App/>, document.getElementById("app"));
