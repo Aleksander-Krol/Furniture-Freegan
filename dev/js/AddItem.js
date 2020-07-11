d13062a57b16acca8b4a54d548ea9b1cd5e754c6
Aleksander-Krol <62897137+Aleksander-Krol@users.noreply.github.com>
2020-07-11 17:04:05 +0200

    form skeleton and implementation of firebase

--- dev/js/AddItem.js
+++ dev/js/AddItem.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 
 export const AddItem = () => {
     return (
-       <form>Hello
+       <form>
+           <h1>Dodaj przedmiot</h1>
+           <input type={Text} placeholder={"Nazwa rzeczy"} ></input>
+           <textarea> </textarea>
+           <input type={File} placeholder={"Dodaj zdjęcie"}></input>
+           <button>Dodaj</button>
 
            
        </form>
