374e40ac1cfc8310a62ff0d991bf0ba5e38717b2
Aleksander-Krol <62897137+Aleksander-Krol@users.noreply.github.com>
2020-07-17 19:00:59 +0200

    branch

--- dev/js/AddItem.js
+++ dev/js/AddItem.js
@@ -2,8 +2,21 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
 export const AddItem = () => {
+    
+    const formStyle = {
+        display: "flex",
+        width: "400px",
+        margin: "0 auto",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        border: "1px solid #cacaca",
+        borderRadius: "20px",
+        padding: "40px"
+    }
+    
     return (
-       <form>
+       <form style={formStyle}>
            <h1>Dodaj przedmiot</h1>
            <input type={Text} placeholder={"Nazwa rzeczy"} ></input>
            <textarea> </textarea>
--- dev/js/app.js
+++ dev/js/app.js
@@ -1,6 +1,21 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import {AddItem} from "./AddItem";
+import firebase from "firebase";
+// import firebase from 'firebase/app';
+// import firebase from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBfK-sd_jo1kRHzGy0lmCQbEpO_LtKbjhU",
+    authDomain: "furniture-freegan.firebaseapp.com",
+    databaseURL: "https://furniture-freegan.firebaseio.com",
+    projectId: "furniture-freegan",
+    storageBucket: "furniture-freegan.appspot.com",
+    messagingSenderId: "782067447286",
+    appId: "1:782067447286:web:e1ad0ca203aed44ec283d7",
+    measurementId: "G-1X7C507TM0"
+  };
+firebase.initializeApp(firebaseConfig);
 
 const App = () => <AddItem/>
 ReactDOM.render(<App/>, document.getElementById("app"));
